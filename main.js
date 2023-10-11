6e90313687cd0251b68edb404bf87c16efbbc0c2
Diego Mendoza <diegom.mendoza2601@gmail.com>
2023-10-11 14:11:56 -0400

    implementacion y animacion del visualizador.js en un lienzo adyacente al camino

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-const lienzo = document.getElementById("miLienzo");
-lienzo.width = 200;
+const lienzoCarro = document.getElementById("miLienzo");
+lienzoCarro.width = 200;
 
-const ctx = lienzo.getContext("2d");
+const lienzoRedNeuronal = document.getElementById("lienzoRedNeuronal");
+lienzoRedNeuronal.width = 300;
 
-const camino = new Road(lienzo.width / 2, lienzo.width * 0.9);
+const ctxCarro = lienzoCarro.getContext("2d");
+const ctxRed = lienzoRedNeuronal.getContext("2d");
+
+const camino = new Road(lienzoCarro.width / 2, lienzoCarro.width * 0.9);
 
 // const carro = new Car(100, 100, 30, 50);
 //! El constructor de la clase Carro contará con 2 parámetros adicionales el primero servirá para establecer qué instancia especificamente será controlada por el usuario a través de las teclas de dirección y qué instancia conducirá de forma autónoma sobre el lienzo
@@ -17,26 +21,32 @@ const trafico = [new Car(camino.obtenerCentroCarril(1), -100, 30, 50, "TRAFICO",
 
 animar();
 
-function animar() {
+function animar(time) {
   for (let i = 0; i < trafico.length; i++) {
     trafico[i].actualizar(camino.bordes, []);
   }
 
   carro.actualizar(camino.bordes, trafico);
 
-  lienzo.height = window.innerHeight;
+  lienzoCarro.height = window.innerHeight;
+  lienzoRedNeuronal.height = window.innerHeight;
 
-  ctx.save();
+  ctxCarro.save();
 
   //! desplazamiento en tiempo real del lienzo y del punto origen sincronizado al desplazamiento del vehículo
-  ctx.translate(0, -carro.y + lienzo.height * 0.7);
+  ctxCarro.translate(0, -carro.y + lienzoCarro.height * 0.7);
 
-  camino.dibujar(ctx);
+  camino.dibujar(ctxCarro);
   for (let i = 0; i < trafico.length; i++) {
-    trafico[i].dibujar(ctx, "red");
+    trafico[i].dibujar(ctxCarro, "red");
   }
-  carro.dibujar(ctx, "blue");
+  carro.dibujar(ctxCarro, "blue");
 
-  ctx.restore();
+  ctxCarro.restore();
+
+  //animar visualizador mediante el argumento time el cual es provisto a través del método requestAnimationFrame
+  ctxRed.lineDashOffset = -time / 50;
+  Visualizer.drawNetwork(ctxRed, carro.cerebro);
+
   requestAnimationFrame(animar);
 }
--- recursos.js
+++ recursos.js
@@ -34,3 +34,11 @@ function interseccionPoligono(poligono1, poligono2) {
   }
   return false;
 }
+
+function getRGBA(value) {
+  const alpha = Math.abs(value);
+  const R = value < 0 ? 0 : 255;
+  const G = R;
+  const B = value > 0 ? 0 : 255;
+  return "rgba(" + R + "," + G + "," + B + "," + alpha + ")";
+}
